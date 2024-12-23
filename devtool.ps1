6af600a031d2ab914854d85a6dfbdeba5d33d97f
ProgramZelva <programzelva@gmail.com>
2024-12-23 09:55:50 +0100

    Update devtool.ps1

--- devtool.ps1
+++ devtool.ps1
@@ -1,4 +1,10 @@
 Write-Output "Starting ProgramZelva ToolBox..."
+Write-Output "Hledám soubor zde: $applicationsFilePath"
+if (-not (Test-Path $applicationsFilePath)) {
+    Write-Error "Soubor applications.json nenalezen. Aktuální adresář: $(Get-Location)"
+    return
+}
+
 
 # Nacteni WPF
 Add-Type -AssemblyName PresentationFramework
